2df94a4222cfa1b35a5855ea64db7125d376ff3e
Master Yoda Rossi <saulohr@yahoo.com.br>
2024-05-30 14:00:17 -0300

    update

--- teste.js
+++ teste.js
@@ -1,30 +1,8 @@
 
 
-
-
-let meses = 1
-
-
-/*while (economiaTotal < 15000) {
-  let economiaMes = (200 * 1.008 ** meses) - 72;
-  economiaTotal += economiaMes;
-  console.log(economiaTotal)
-  meses++
-  console.log(meses)
-}
-console.log(meses)
-*/
-
-
-
-
-function economia(contaM, painelS) {
-  let economiaTotal = 0;
-  while (economiaTotal < painelS) {
-    let economiaMes = (contaM * 1.008 ** meses) - 72;
-    economiaTotal += economiaMes;
-    meses++
+for (let y = 0; y <= 100; y++) {
+  if (Number.isInteger(255*y/100) == true){
+    console.log(`${y} - ${255*y/100}`)
   }
-  return meses
 }
-console.log(economia(250, 15000))
+
